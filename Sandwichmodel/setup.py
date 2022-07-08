f46dc6658b8a6de70e06100ae87b3fa6f6dfffa5
Andrin Bertschi <abertschi@abertschi.ch>
2022-07-08 21:13:28 +0200

    add setup.py package

--- Sandwichmodel/setup.py
+++ Sandwichmodel/setup.py
@@ -2,14 +2,13 @@ import setuptools
 
 
 setuptools.setup(
-     name='Sandwichmodel',
-     version='0.1',
-     author="StrucEngLib",
-     description="",
-     url="https://github.com/kfmResearch-NumericsTeam/StrucEng_Library",
-    packages=["Sandwichmodel.Sandwichmodel", "Sandwichmodel.Printerfunctions"],
+    name='strucenglib_sandwichmodel',
+    version='0.1',
+    author="StrucEngLib",
+    description="",
+    url="https://github.com/kfmResearch-NumericsTeam/StrucEng_Library",
+    packages=['Sandwichmodel', 'Printerfunctions'],
     package_dir={
-        'Sandwichmodel': 'Codes',
+        '': 'Codes',
     }
  )
-
