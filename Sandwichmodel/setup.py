fba8a33bcbbb3bd91ae88bcc4c0d0696945ae486
Andrin Bertschi <abertschi@abertschi.ch>
2022-07-08 18:53:34 +0200

    convert into pip package

---
+++ Sandwichmodel/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+setuptools.setup(
+     name='Sandwichmodel',
+     version='0.1',
+     author="StrucEngLib",
+     description="",
+     url="https://github.com/kfmResearch-NumericsTeam/StrucEng_Library",
+    packages=['Sandwichmodel'],
+    package_dir={'Sandwichmodel':'Codes'}
+ )
